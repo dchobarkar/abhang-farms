ba66c6d26e98365832907a54b60904d54a585c8d
Darshan Chobarkar <dchobarkar@gmail.com>
2021-11-30 13:44:59 +0530

    Css improved

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./index.css";
 import "./fonts.css";
 
@@ -16,7 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
