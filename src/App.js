dc24e8ab51e98e9c22711135a8f60603a51e42ff
Darshan Chobarkar <dchobarkar@gmail.com>
2021-11-30 14:08:26 +0530

    Routes moved in App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
+import NavBar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
-import './App.css';
+import Spinner from "./customComponents/Spinner/Spinner";
+
+const LandingPage = React.lazy(() => {
+  return import("./components/LandingPage/LandingPage");
+});
+const Gallery = React.lazy(() => {
+  return import("./components/Gallery/Gallery");
+});
+const Booking = React.lazy(() => {
+  return import("./components/Booking/Booking");
+});
+const NotFound = React.lazy(() => {
+  return import("./components/NotFound/NotFound");
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Layout />
-      </div>
+      <NavBar />
+
+      <Suspense fallback={Spinner}>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => <LandingPage {...props} />}
+          />
+
+          <Route
+            path="/gallery"
+            exact
+            render={(props) => <Gallery {...props} />}
+          />
+
+          <Route
+            path="/booking"
+            exact
+            render={(props) => <Booking {...props} />}
+          />
+
+          <Route render={(props) => <NotFound {...props} />} />
+        </Switch>
+      </Suspense>
+
+      <Footer />
     </BrowserRouter>
   );
 }
